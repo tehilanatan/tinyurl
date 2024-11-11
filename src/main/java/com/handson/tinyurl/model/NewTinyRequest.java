a1776fc3ce59cf5f92df0b48659342911ac17018
tehilanatan <tehila.natan1@gmail.com>
2024-11-11 14:50:00 +0200

    Initial commit

---
+++ src/main/java/com/handson/tinyurl/model/NewTinyRequest.java
@@ -0,0 +1,15 @@
+package com.handson.tinyurl.model;
+
+public class NewTinyRequest {
+    private  String longUrl;
+
+    public String getLongUrl() {
+        return longUrl;
+    }
+
+    private  String userName;
+
+    public String getUserName() {
+        return userName;
+    }
+}
